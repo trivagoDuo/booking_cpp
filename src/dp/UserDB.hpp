096115c8396c6a2fbd0f41d524eb83be43bba22c
DLC <shabrov23.02@gmail.com>
2023-05-04 16:02:29 +0300

    Small DTO

---
+++ src/dp/UserDB.hpp
@@ -0,0 +1,75 @@
+//
+// Created by Daniil Shabrov on 5/4/23.
+//
+
+#ifndef BOOKING_CPP_USERDB_HPP
+#define BOOKING_CPP_USERDB_HPP
+
+#include "dto/UserDto.hpp"
+#include "oatpp-postgresql/orm.hpp"
+
+#include OATPP_CODEGEN_BEGIN(DbClient) //<- Begin Codegen
+
+/**
+ * UserDb client definitions.
+ */
+class UserDb : public oatpp::orm::DbClient {
+public:
+
+    UserDb(const std::shared_ptr<oatpp::orm::Executor>& executor)
+            : oatpp::orm::DbClient(executor)
+    {
+
+        oatpp::orm::SchemaMigration migration(executor);
+        migration.addFile(1 /* start from version 1 */, DATABASE_MIGRATIONS "/001_init.sql");
+        // TODO - Add more migrations here.
+        migration.migrate(); // <-- run migrations. This guy will throw on error.
+
+        auto version = executor->getSchemaVersion();
+        OATPP_LOGD("UserDb", "Migration - OK. Version=%d.", version);
+
+    }
+
+    QUERY(createUser,
+    "INSERT INTO AppUser"
+    "(id, username, email, password, role) VALUES "
+    "(uuid_generate_v4(), :user.username, :user.email, :user.password, :user.role)"
+    "RETURNING *;",
+    PREPARE(true), // user prepared statement!
+    PARAM(oatpp::Object<UserDto>, user))
+
+    QUERY(updateUser,
+    "UPDATE AppUser "
+    "SET "
+    " username=:user.username, "
+    " email=:user.email, "
+    " password=:user.password, "
+    " role=:user.role "
+    "WHERE "
+    " id=:user.id "
+    "RETURNING *;",
+    PREPARE(true), //<-- user prepared statement!
+    PARAM(oatpp::Object<UserDto>, user))
+
+    QUERY(getUserById,
+    "SELECT * FROM AppUser WHERE id=:id;",
+    PREPARE(true), //<-- user prepared statement!
+    PARAM(oatpp::String, id))
+
+    QUERY(getAllUsers,
+    "SELECT * FROM AppUser LIMIT :limit OFFSET :offset;",
+    PREPARE(true), //<-- user prepared statement!
+    PARAM(oatpp::UInt32, offset),
+    PARAM(oatpp::UInt32, limit))
+
+    QUERY(deleteUserById,
+    "DELETE FROM AppUser WHERE id=:id;",
+    PREPARE(true), //<-- user prepared statement!
+    PARAM(oatpp::String, id))
+
+};
+
+#include OATPP_CODEGEN_END(DbClient) //<- End Codegen
+
+
+#endif //BOOKING_CPP_USERDB_HPP
---
+++ src/dto/StatusDto.hpp
@@ -0,0 +1,35 @@
+//
+// Created by Daniil Shabrov on 5/4/23.
+//
+
+#ifndef BOOKING_CPP_STATUSDTO_HPP
+#define BOOKING_CPP_STATUSDTO_HPP
+
+#include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/Types.hpp"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+class StatusDto : public oatpp::DTO {
+
+    DTO_INIT(StatusDto, DTO)
+
+    DTO_FIELD_INFO(status) {
+            info->description = "Short status text";
+    }
+    DTO_FIELD(String, status);
+
+    DTO_FIELD_INFO(code) {
+            info->description = "Status code";
+    }
+    DTO_FIELD(Int32, code);
+
+    DTO_FIELD_INFO(message) {
+            info->description = "Verbose message";
+    }
+    DTO_FIELD(String, message);
+
+};
+
+#include OATPP_CODEGEN_END(DTO)
+#endif //BOOKING_CPP_STATUSDTO_HPP
